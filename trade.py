e55a6a422bf3a38280da8f9f6ba5390212b0a0ab
firewang666 <61103073+firewang666@users.noreply.github.com>
2024-09-26 01:22:59 -0400

    Update trade.py

--- trade.py
+++ trade.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route('/run-colab')
 def run_colab():
-    gdown.download('https://drive.google.com/drive/folders/1zVI5huEKOxJn5qB-1vxtuxbGPeAJ6KRh', 'colab.ipynb', quiet=False)
+    gdown.download('https://colab.research.google.com/drive/1q9xxQMu4r2rXtXfbFT98CPAOpSUba05Z?usp=sharing', 'colab.ipynb', quiet=False)
     return jsonify(message='colab notebook ran successfully')
